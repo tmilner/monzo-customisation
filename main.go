37a2529c762b2ded2dcd23ee01c9f1688eda8cc4
Tom Milner <mail@tmilner.co.uk>
2019-02-24 20:42:02 +0000

    Add server for hello world (later for webhooks)

--- main.go
+++ main.go
@@ -5,9 +5,18 @@ import (
 	. "github.com/tmilner/monzo-customisation/httpclient"
 	"log"
 	"net/http"
+	"io"
 )
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
+ 
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	http.ListenAndServe(":8000", mux)
+
 	client := &http.Client{}
 	config, err := New()
 	if err != nil {
