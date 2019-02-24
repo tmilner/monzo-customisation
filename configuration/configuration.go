f12668bfe871b70a0174e77c154c6033d31a09ba
Tom Milner <mail@tmilner.co.uk>
2019-02-24 22:24:21 +0000

    Adding log file

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -1,22 +1,5 @@
 package configuration
 
-import (
-	"encoding/json"
-	"io/ioutil"
-)
-
 type Configuration struct {
-	Authorization string `json:"auth"`
-}
-
-func NewConfig() (*Configuration, error) {
-	contents, err := ioutil.ReadFile("./config.json")
-	if err != nil {
-		return nil, err
-	}
-
-	var configuration Configuration
-	err = json.Unmarshal(contents, &configuration)
-
-	return &configuration, err
+	Authorization string
 }
--- main.go
+++ main.go
@@ -11,13 +11,18 @@ import (
 
 func main() {
 	client := &http.Client{}
-	//config, err := NewConfig()
-	//if err != nil {
-	//	log.Fatalf("Config failed to load %+v", err)
-	//}
 	config := &configuration.Configuration{
 		Authorization: os.Args[1],
 	}
+
+	f, err := os.OpenFile("app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
 	SetupWebhookInterface()
 
 	//whoAmIRes, err := httpclient.WhoAmI(client, config)
