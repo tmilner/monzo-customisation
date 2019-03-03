cddc9832aa0ed8e94fbb108009f01612ab73ede5
Tom Milner <mail@tmilner.co.uk>
2019-03-03 22:49:08 +0000

    Fixed issue and cleaned up basic info

--- httpclient/monzoapi.go
+++ httpclient/monzoapi.go
@@ -50,68 +50,61 @@ func (a *MonzoApi) processGetRequest(path string) ([]byte, error) {
 }
 
 func (a *MonzoApi) runBasicInfo() {
-	//whoAmIRes, err := httpclient.WhoAmI(client, config)
-	//if err != nil {
-	//	log.Println("WhoAmI error", err)
-	//}
-
 	listAccountRes, err := a.ListAccounts()
 	if err != nil {
 		log.Fatalln("ListAccount error", err)
 	}
 
-	log.Println("Retrieving account balances:")
-	for index, account := range listAccountRes.Accounts {
-		balance, err := a.GetBalance(account.Id)
-		if err != nil {
-			log.Fatalln("Error getting balance", err)
-		}
-		log.Printf("%d GetBalance for account %s is %d", index, account.Type, balance.Balance)
-	}
-
+	log.Println("Retrieving pots:")
 	pots, err := a.GetPots()
 	if err != nil {
 		log.Fatalln("GetPots error", err)
 	}
 
-	log.Println("Retrieving pots:")
-	for index, pot := range pots.Pots {
-		var deleted = "active"
-		if pot.Deleted {
-			deleted = "deleted"
+	for _, pot := range pots.Pots {
+		if !pot.Deleted {
+			log.Printf("Found a pot called %s, its got a balence of %d", pot.Name, pot.Balance)
 		}
-		log.Printf("%d: Found a %s pot called %s, its got a balence of %d and is currently using the style %s", index, deleted, pot.Name, pot.Balance, pot.Style)
 	}
 
-	log.Println("Lets get the transactions for all accounts:")
+	log.Println("Running through accounts:")
 
 	for _, account := range listAccountRes.Accounts {
-		//transactions, err := a.GetTransactions(account.Id)
-		//if err != nil {
-		//	log.Fatalln("Error getting transactions", err)
-		//}
-		//
-		//domain, err := transactions.ToDomain()
-		//if err != nil {
-		//	log.Fatalln("Error converting to domain type")
-		//}
-		//RankAndPrintMerchants(domain)
-		params := Params{
-			Title:    "Starting Service",
-			Body:     "Service is starting",
-			ImageUrl: "https://d33wubrfki0l68.cloudfront.net/673084cc885831461ab2cdd1151ad577cda6a49a/92a4d/static/images/favicon.png",
-		}
+		if !account.Closed {
+			balance, err := a.GetBalance(account.Id)
+			if err != nil {
+				log.Println("Error getting balance", err)
+			}
+			log.Printf("Balance for account %s is %d", account.Type, balance.Balance)
 
-		feedItem := &FeedItem{
-			TypeParam: "basic",
-			AccountId: account.Id,
-			Url:       "http://tmilner.co.uk",
-			Params:    params,
-		}
-		log.Printf("Creating a feed item %+v", feedItem)
-		feedErr := a.CreateFeedItem(feedItem)
-		if feedErr != nil {
-			log.Fatalf("Feed error!! %+v", feedErr)
+			//transactions, err := a.GetTransactions(account.Id)
+			//if err != nil {
+			//	log.Fatalln("Error getting transactions", err)
+			//}
+			//
+			//domain, err := transactions.ToDomain()
+			//if err != nil {
+			//	log.Fatalln("Error converting to domain type")
+			//}
+			//RankAndPrintMerchants(domain)
+
+			params := Params{
+				Title:    "Starting Service",
+				Body:     "Service is starting",
+				ImageUrl: "https://d33wubrfki0l68.cloudfront.net/673084cc885831461ab2cdd1151ad577cda6a49a/92a4d/static/images/favicon.png",
+			}
+
+			feedItem := &FeedItem{
+				TypeParam: "basic",
+				AccountId: account.Id,
+				Url:       "http://tmilner.co.uk",
+				Params:    params,
+			}
+			log.Printf("Creating a feed item %+v", feedItem)
+			feedErr := a.CreateFeedItem(feedItem)
+			if feedErr != nil {
+				log.Println("Feed error!! %+v", feedErr)
+			}
 		}
 	}
 }
