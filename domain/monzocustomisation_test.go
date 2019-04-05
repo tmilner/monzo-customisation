1e717426506d15d49d551ffbfe6aa6af4a9976e9
Tom Milner <tmilner@mdsol.com>
2019-04-05 20:35:36 +0100

    Tests

---
+++ domain/monzocustomisation_test.go
@@ -0,0 +1,173 @@
+package domain
+
+import (
+	"github.com/tmilner/monzo-customisation/monzoclient"
+	"github.com/twinj/uuid"
+	"reflect"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCreateMonzoApi(t *testing.T) {
+	type args struct {
+		config *Config
+		client *monzoclient.MonzoClient
+	}
+
+	config := &Config{}
+	client := &monzoclient.MonzoClient{}
+
+	tests := []struct {
+		name string
+		args args
+		want *MonzoCustomisation
+	}{
+		{
+			name: "Values are initialised correct",
+			args: args{
+				config: config,
+				client: client,
+			},
+			want: &MonzoCustomisation{
+				client:       client,
+				config:       config,
+				users:        map[string]*User{},
+				usersLock:    sync.RWMutex{},
+				accounts:     map[string]*Account{},
+				accountsLock: sync.RWMutex{},
+				ticker:       time.NewTicker(2 * time.Hour),
+				stateToken:   uuid.NewV4().String(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := CreateMonzoCustomisation(tt.args.client, tt.args.config)
+			got.stateToken = tt.want.stateToken
+			got.ticker = tt.want.ticker
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CreateMonzoCustomisation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMonzoApi_findUserForAccount(t *testing.T) {
+	type fields struct {
+		url          string
+		client       *monzoclient.MonzoClient
+		clientConfig *Config
+		users        map[string]*User
+		usersLock    sync.RWMutex
+		accounts     map[string]*Account
+		accountsLock sync.RWMutex
+	}
+	type args struct {
+		accountId string
+	}
+
+	accountId := "12345"
+	user := &User{}
+	account := &Account{
+		id:   accountId,
+		user: user,
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *User
+		wantErr bool
+	}{
+		{
+			name: "Return the user details when an account is found",
+			fields: fields{
+				accountsLock: sync.RWMutex{},
+				accounts: map[string]*Account{
+					accountId: account,
+				},
+			},
+			args:    args{accountId: accountId},
+			want:    user,
+			wantErr: false,
+		},
+		{
+			name: "Returns an error when the account ID cannot be found",
+			fields: fields{
+				accountsLock: sync.RWMutex{},
+				accounts:     map[string]*Account{},
+			},
+			args:    args{accountId: accountId},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &MonzoCustomisation{
+				client:       tt.fields.client,
+				config:       tt.fields.clientConfig,
+				users:        tt.fields.users,
+				usersLock:    tt.fields.usersLock,
+				accounts:     tt.fields.accounts,
+				accountsLock: tt.fields.accountsLock,
+			}
+			got, err := a.findUserForAccount(tt.args.accountId)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MonzoCustomisation.findUserForAccount() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MonzoCustomisation.findUserForAccount() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+/*func TestMonzoApi_runBasicInfo(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	type fields struct {
+		client       *monzoclient.MonzoClient
+		config       *Config
+		users        map[string]*User
+		usersLock    sync.RWMutex
+		accounts     map[string]*Account
+		accountsLock sync.RWMutex
+	}
+	type args struct {
+		userId string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name:   "",
+			fields: fields{
+				client:
+			},
+			args:   args{
+				userId: "23232",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &MonzoCustomisation{
+				client:       tt.fields.client,
+				config:       tt.fields.config,
+				users:        tt.fields.users,
+				usersLock:    tt.fields.usersLock,
+				accounts:     tt.fields.accounts,
+				accountsLock: tt.fields.accountsLock,
+			}
+			a.runBasicInfo(tt.args.userId)
+		})
+	}
+}
+*/
---
+++ monzoclient/monzoclient_test.go
@@ -0,0 +1,101 @@
+package monzoclient
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestCreateMonzoClient(t *testing.T) {
+	type args struct {
+		url    string
+		client *http.Client
+	}
+	url := "monzo"
+	client := &http.Client{}
+	tests := []struct {
+		name string
+		args args
+		want *MonzoClient
+	}{
+		{
+			name: "Client is created successfully",
+			args: args{url, client},
+			want: &MonzoClient{url, client},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CreateMonzoClient(tt.args.url, tt.args.client); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CreateMonzoClient() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMonzoClient_processGetRequest(t *testing.T) {
+	type args struct {
+		path      string
+		authToken string
+	}
+	authToken := "9876"
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+		server  *httptest.Server
+	}{
+		{
+			name: "Calls the API and returns the response as a byte array if the user is authenticated.",
+			args: args{
+				path:      "/test",
+				authToken: authToken,
+			},
+			want:    []byte(`hello`),
+			wantErr: false,
+			server: httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.Header.Get("Authorization") != "Bearer "+authToken {
+					http.Error(w, "Error", http.StatusUnauthorized)
+				} else {
+					_, _ = w.Write([]byte(`hello`))
+				}
+
+			})),
+		},
+		{
+			name: "Calls the API and returns an error if the server returns a non 200 code",
+			args: args{
+				path:      "/test",
+				authToken: authToken,
+			},
+			want:    nil,
+			wantErr: true,
+			server: httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				http.Error(w, "Error", http.StatusInternalServerError)
+			})),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.server != nil {
+				defer tt.server.Close()
+				tt.server.Start()
+			}
+
+			a := &MonzoClient{
+				url:    tt.server.URL,
+				client: &http.Client{},
+			}
+			got, err := a.processGetRequest(tt.args.path, tt.args.authToken)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MonzoClient.processGetRequest() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MonzoClient.processGetRequest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
